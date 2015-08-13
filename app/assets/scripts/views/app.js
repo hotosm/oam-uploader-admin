5096a8f4dfa480dc5f9e92df9a8435798db9bf79
Ricardo Mestre <ricardo@duplos.org>
2015-08-13 12:00:02 +0100

    Style and structure signed in header

--- app/assets/scripts/views/app.js
+++ app/assets/scripts/views/app.js
@@ -12,6 +12,12 @@ var App = React.createClass({
             <div className="site-headline">
               <h1 className="site-title"><img src="assets/graphics/layout/oam-logo-h-pos.svg" width="167" height="32" alt="OpenAerialMap logo" /><span>OpenAerialMap</span> <small>Token Manager</small></h1>
             </div>
+            <nav className="site-prime-nav">
+              <h2><small>Signed in as</small> ricardo@developmentseed.org</h2>
+              <ul className="meta-menu">
+                <li><a href="#" className="bttn-signout" title="Sign out"><span>Sign out</span></a></li>
+              </ul>
+            </nav>
           </div>
         </header>
         <main className="site-body" role="main">
